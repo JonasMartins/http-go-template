a2d65350e3f938b6c1b50cad010af25dc0289bfe
JonasMartins <jonas.w.martins@gmail.com>
2023-08-05 14:27:53 -0300

    add: Fix testing commands
     Changes to be committed:
    	new file:   .DS_Store
    	deleted:    .gitignore
    	new file:   project/.DS_Store
    	renamed:    .editorconfig -> project/.editorconfig
    	new file:   project/.gitignore
    	renamed:    Makefile -> project/Makefile
    	modified:   project/go.sum
    	modified:   project/test/config_test.go

--- project/test/config_test.go
+++ project/test/config_test.go
@@ -1,52 +1,52 @@
 package test
 
 import (
+	"log"
 	"project/src/pkg/utils"
 	"project/src/services/main/configs"
 	"reflect"
 	"testing"
 )
 
-func TestGetFilePath(t *testing.T) {
-
-	u := utils.New()
-	f, err := u.GetFilePath(&[]string{"src", "services", "main", "configs", "base.yaml"})
-	if err != nil {
-		t.Logf("Err %v", err)
-	}
-
-	t.Log(f)
-	got, want := 1, 1
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
-	}
-}
-
-func TestGetRoot(t *testing.T) {
+func TestConfig(t *testing.T) {
+	defer func() {
+		log.Println("Deferred tearing down.")
+	}()
 
 	u := utils.New()
 
-	got, err := u.GetRelativeRootDir()
-	if err != nil {
-		t.Logf("Err %v", err)
-	}
+	t.Run("Get FilePath", func(t *testing.T) {
+		f, err := u.GetFilePath(&[]string{"src", "services", "main", "configs", "base.yaml"})
+		if err != nil {
+			t.Logf("Err %v", err)
+		}
+		t.Log(f)
+		got, want := 1, 1
+		if got != want {
+			t.Errorf("got %d, wanted %d", got, want)
+		}
+	})
 
-	want := "project"
-	if *got != want {
-		t.Errorf("got %s, wanted %s", *got, want)
-	}
+	t.Run("Get Root", func(t *testing.T) {
+		got, err := u.GetRelativeRootDir()
+		if err != nil {
+			t.Logf("Err %v", err)
+		}
+		want := "project"
+		if *got != want {
+			t.Errorf("got %s, wanted %s", *got, want)
+		}
+	})
 
-}
-
-func TestLoadConfig(t *testing.T) {
-
-	var want configs.Config
-	got, err := configs.LoadConfig()
-	if err != nil {
-		t.Errorf("Error %s", err.Error())
-	}
-	if reflect.TypeOf(got) != reflect.TypeOf(&want) {
-		t.Errorf("got %q, wanted %q", *got, want)
-	}
-	got = nil
+	t.Run("Load config", func(t *testing.T) {
+		var want configs.Config
+		got, err := configs.LoadConfig()
+		if err != nil {
+			t.Errorf("Error %s", err.Error())
+		}
+		if reflect.TypeOf(got) != reflect.TypeOf(&want) {
+			t.Errorf("got %q, wanted %q", *got, want)
+		}
+		got = nil
+	})
 }
