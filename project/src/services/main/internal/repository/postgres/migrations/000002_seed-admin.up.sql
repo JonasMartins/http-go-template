c7e3a65ccd963f95cf88f5dcf841c09a4625f09a
JonasMartins <jonas.w.martins@gmail.com>
2023-08-22 20:17:51 -0300

    fix: Fix migrations on wrong seed procedure
     Changes to be committed:
    	modified:   project/src/services/main/internal/repository/postgres/migrations/000002_seed-admin.up.sql

--- project/src/services/main/internal/repository/postgres/migrations/000002_seed-admin.up.sql
+++ project/src/services/main/internal/repository/postgres/migrations/000002_seed-admin.up.sql
@@ -11,7 +11,7 @@ begin
 
 	if not found then
 		insert into public.users (uuid, name, email, "password")
-		values ('4a9b3fd5-6813-4c75-9598-5fd9ae202d88','$2a$10$zYC48a1doguo1VoCqbmQBezAUQJKVSbGnHgoPWInNFn2idbPABUoe', 'admin@email.com', 'admin');
+		values ('4a9b3fd5-6813-4c75-9598-5fd9ae202d88','Admin', 'admin@email.com', '$2a$10$zYC48a1doguo1VoCqbmQBezAUQJKVSbGnHgoPWInNFn2idbPABUoe');
 		raise notice 'admin user was sown';
 	else
 		raise notice 'user: % already exist', admin.name;
